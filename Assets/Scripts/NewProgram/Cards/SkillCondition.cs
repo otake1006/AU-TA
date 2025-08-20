8b0894c643bc21ae07b8b29457b8003f0b6a7ab6
Elic0de <lpondboy@gmail.com>
2025-08-20 11:04:55 +0900

    wip SkillConditionをScriptableObjectとして実装

--- Assets/Scripts/NewProgram/Cards/SkillCondition.cs
+++ Assets/Scripts/NewProgram/Cards/SkillCondition.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-[System.Serializable]
-public class SkillCondition
+[CreateAssetMenu(fileName = "New Condition Card", menuName = "Card Game/Basic Condition Card")]
+public class SkillCondition : ScriptableObject
 {
     [Header("ğŒİ’è")]
     public ConditionType conditionType;
